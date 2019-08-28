09e041a96927a5e6b6f2114428493bfe8acd147b
salem-alkaabi97 <salem-alkaabi97@hotmail.com>
2019-08-28 18:12:25 +0400

    HTML and CSS

--- BasicHTMLCSS.js
+++ BasicHTMLCSS.js
@@ -3,6 +3,28 @@
 //   { name: "Salem", car: "VW" },
 //   { name: "Magic", car: "Bicycle" }
 // ];
+// var container = document.getElementById("container");
+// for (let obj of test) {
+//   for (let prop in obj) {
+//     var value = obj[prop];
+//     var div = document.createElement("div");
+//     div.innerHTML = value;
+//     container.appendChild(div);
+//   }
+// }
+//{
+///
+// var container = document.getElementById("container");
+// var div = document.createElement("div");
+// div1.innerHTML = obj[0].car;
+
+// var div1 = document.createElement("div1");
+// container.appendChild(div1);
+
+// var obj = { name: "Ashish", car: "Fortuner" };
+// for (let prop in obj) {
+//   var value = obj[prop];
+// }
 var test = [
   {
     product_id: "1",
@@ -32,41 +54,40 @@ var test = [
     description: "A gaming chair meant for long-term sitting time."
   }
 ];
-// var container = document.getElementById("container");
-// for (let obj of test) {
-//   for (let prop in obj) {
-//     var value = obj[prop];
-//     var div = document.createElement("div");
-//     div.innerHTML = value;
-//     container.appendChild(div);
-//   }
-// }
-{
-  /* <div class="gallery">
-<a target="_blank" href="./assets/gamingchair1.png"></a>
-<img src="./assets/gamingchair1.png" alt="gamingchair.png">
-</a>
-<div class="desc">DXRacer Gaming Chair Black/Yellow<br> Price: 1,200 AED</div>
-</div> */
-}
 
+// * <div class="gallery">
+// <a target="_blank" href="./assets/gamingchair1.png"></a>
+// <img src="./assets/gamingchair1.png" alt="gamingchair.png">
+// </a>
+// <div class="desc">DXRacer Gaming Chair Black/Yellow<br> Price: 1,200 AED</div>
+// </div> */
+// //}
 var container = document.getElementById("container");
 for (let obj of test) {
+  // div gallery has img tag only
   var gallery = document.createElement("div");
   gallery.className = "gallery";
+
+  //create tag img
+  var img = document.createElement("img");
+  img.src = obj.imageURL;
+  gallery.appendChild(img);
+
+  //div description has description only
   var description = document.createElement("div");
   description.className = "desc";
   description.innerHTML = getDescription(obj);
   gallery.appendChild(description);
-  for (let prop in obj) {
-    // create gallery
-    // if (prop == "name") {
-    // }
-    // var value = obj[prop];
-    // var div = document.createElement("div");
-    // div.innerHTML = value;
-    // container.appendChild(gallery);
-  }
+
+  // for (let prop in obj) {
+  //   // create gallery
+  //   // if (prop == "name") {
+  //   // }
+  //   // var value = obj[prop];
+  //   // var div = document.createElement("div");
+  //   // div.innerHTML = value;
+  //   // container.appendChild(gallery);
+  // }
   container.appendChild(gallery);
 }
 function getDescription(element) {
@@ -81,14 +102,3 @@ function getDescription(element) {
     element.description
   );
 }
-// var container = document.getElementById("container");
-// var div = document.createElement("div");
-// div1.innerHTML = obj[0].car;
-
-// var div1 = document.createElement("div1");
-// container.appendChild(div1);
-
-// var obj = { name: "Ashish", car: "Fortuner" };
-// for (let prop in obj) {
-//   var value = obj[prop];
-// }
--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-// fetch("./products.json")
-//   .then(response => response.json())
-//   .then(data => createTable(data.products));
-
-var empData;
+var prodData;
 var promise = fetch("./products.json");
 promise
   .then(response => {
     return response.json();
   })
   .then(data => {
-    empData = data;
-    createTable(data);
+    prodData = data;
+    createProducts(data);
   });
-function createTable(records) {
-  var table = document.createElement("table");
-  table.setAttribute("border", 1);
-  table.appendChild(createHeading(records[0]));
-  for (let record of records) {
-    table.appendChild(createRow(record));
+function createProducts(test) {
+  // we will get the id of the middle panel here
+  var middlePanel = document.getElementById("flex-item-middle-panel");
+  for (let obj of test) {
+    // div gallery has img tag only
+    var gallery = document.createElement("div");
+    gallery.className = "gallery";
+
+    //create tag img and assign each URL to it
+    var img = document.createElement("img");
+    img.src = obj.imageURL;
+    // append this img to the parent div gallery
+    gallery.appendChild(img);
+
+    //div description has description only
+    var description = document.createElement("div");
+    description.className = "desc";
+    description.innerHTML = getDescription(obj);
+    gallery.appendChild(description);
+
+    middlePanel.appendChild(gallery);
   }
-  document.getElementById("container").appendChild(table);
 }
-function createHeading(record) {
-  var row = document.createElement("tr");
-  for (let prop in record) {
-    var heading = document.createElement("th");
-    heading.innerHTML = prop.toUpperCase();
-    row.appendChild(heading);
-  }
-  return row;
-}
-function createRow(record) {
-  var row = document.createElement("tr");
-  for (let prop in record) {
-    var column = document.createElement("td");
-    column.innerHTML = record[prop];
-    row.appendChild(column);
-  }
-  return row;
+function getDescription(element) {
+  return (
+    "Name: " +
+    element.name +
+    "<br>" +
+    " Color: " +
+    element.color +
+    "<br>" +
+    " Price: " +
+    element.price +
+    "<br>" +
+    " Description: " +
+    element.description
+  );
 }
