53176c0e3ac99cdde532e869d71231f78e0a217b
salem-alkaabi97 <salem-alkaabi97@hotmail.com>
2019-09-01 08:47:32 +0400

    HTML and CSS

--- login-js.js
+++ login-js.js
@@ -1,7 +1,30 @@
-function check(form) {
-  if (form.userid.value == "123" && form.pswrd.value == "123") {
-    location.replace("main.html");
-  } else {
+var usersData;
+var promise = fetch("./users.json");
+promise
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    usersData = data;
+  });
+
+function validateLogin() {
+  var username = document.getElementById("userid").value;
+  var password = document.getElementById("pswrd").value;
+  for (value of usersData) {
+    if (value.username == username && value.password == password) {
+      localStorage.setItem("username", username);
+      localStorage.setItem(username, localStorage.getItem(username));
+      location.replace("main.html");
+    }
     alert("The password or the username is incorrect, try again.");
   }
 }
+
+// function check(form) {
+//   if (form.userid.value == "123" && form.pswrd.value == "123") {
+//     location.replace("main.html");
+//   } else {
+//     alert("The password or the username is incorrect, try again.");
+//   }
+// }
